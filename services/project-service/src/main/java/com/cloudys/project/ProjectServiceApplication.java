42eb76b591f236619c60cc6506fad584d61240e3
2301_81624400 <2301_81624400@noreply.gitcode.com>
2026-06-24 06:30:59 -0400

    fin

--- services/project-service/src/main/java/com/cloudys/project/ProjectServiceApplication.java
+++ services/project-service/src/main/java/com/cloudys/project/ProjectServiceApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
+@SpringBootApplication   
+@EnableDiscoveryClient// ← 启用服务发现（向 Eureka 注册）
+@EnableFeignClients// ← 启用 Feign 客户端扫描
 public class ProjectServiceApplication {
 
     public static void main(String[] args) {
